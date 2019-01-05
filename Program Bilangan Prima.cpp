9e5789836ed7d28f6b6d50ad67d2244a7bb8fec4
AminPriadi <45525407+AminPriadi@users.noreply.github.com>
2019-01-05 11:38:10 +0700

    Add files via upload

---
+++ Program Bilangan Prima.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"menampilkan bilangan prima dari 1-100\n ";
+    for(a=1;a<=100;a++)
+    {
+        c=0;
+        for(b=1;b<=a;b++)
+        {
+            if(a%b==0){c+=1;}
+        }
+        if(c==2)
+        {
+            cout<<a<< " ";
+        }
+    }
+    getch();
+    return 0;
+}
